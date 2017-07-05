77ee21eaa995f4d309484d389bb3a6f36fbced3a
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-05 13:01:51 -0700

    Added random number generator.

--- game.py
+++ game.py
@@ -4,3 +4,5 @@ import random
 
 name = raw_input("Hey! What's your name? ")
 number = raw_input(("%s, I'm thinking of a number between 1 and 100.\nTry to guess my number.\nYour guess? ")%(name))
+random_number = random.randint(1,100)
+print random_number
