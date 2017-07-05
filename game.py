9797efba9c4ef6f0b2b6582073b7eb0501d47d5b
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-05 16:13:10 -0700

    completed the while loop/the game

--- game.py
+++ game.py
@@ -6,6 +6,16 @@ name = raw_input("Hey! What's your name? ")
 print "%s, I'm thinking of a number between 1 and 100.\nTry to guess my number." % (name)
 
 random_number = random.randint(1, 100)
+count = 0
 
 while True:
     number_guess = int(raw_input("Your guess? "))
+    if number_guess != random_number:
+        count += 1
+        if number_guess < random_number:
+            print "Your guess is too low, try again."
+        else:
+            print "Your guess is too high, try again."
+    else:
+        print "Congrats! You guessed correctly, after %d guesses." % (count)
+        break
