d03ccbfe0794396cbcaf15bd5d4bd9c70bd757a4
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-05 17:12:47 -0700

    added a try and except statement

--- game.py
+++ game.py
@@ -9,14 +9,23 @@ random_number = random.randint(1, 100)
 count = 0
 
 while True:
-    number_guess = int(raw_input("Your guess? "))
-    if number_guess != random_number:
-        count += 1
-        if number_guess < random_number:
-            print "Your guess is too low, try again."
+    try:    
+        number_guess = int(raw_input("Your guess? "))
+        if number_guess != random_number:
+            count += 1
+            if number_guess < random_number:
+                if number_guess < 0:
+                    print "That is not a valid number. Please guess between 1 and 100."
+                else:
+                    print "Your guess is too low, try again."
+            else:
+                if number_guess > 100:
+                    print "That is not a valid number. Please guess between 1 and 100."
+                else:
+                    print "Your guess is too high, try again."
         else:
-            print "Your guess is too high, try again."
-    else:
-        count += 1
-        print "Congrats! You guessed correctly, after %d guesses." % (count)
-        break
+            count += 1
+            print "Congrats! You guessed correctly, after %d guesses." % (count)
+            break
+    except ValueError:
+        print "Please enter a valid interger."
