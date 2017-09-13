440fb88e198c955c40b028c279810005c72f40f2
vagrant <vagrant@vagrant.vm>
2017-09-13 00:51:51 +0000

    put the game logic in a function definition

--- game.py
+++ game.py
@@ -5,25 +5,26 @@ import random
 name = raw_input("Hey! What's your name? ")
 print "%s, I'm thinking of a number between 1 and 100.\nTry to guess my number." % (name)
 
-random_number = random.randint(1, 100)
-count = 0
+def play_game(): 
+    random_number = random.randint(1, 100)
+    count = 0
 
-while True:
-    count += 1
+    while True:
+        count += 1
 
-    try:
-        number_guess = int(raw_input("Your guess? "))
+        try:
+            number_guess = int(raw_input("Your guess? "))
 
-    except ValueError:
-        print "Please enter a valid interger."
-        continue
+        except ValueError:
+            print "Please enter a valid interger."
+            continue
 
-    if number_guess < 0 or number_guess > 100:
-        print "That is not a valid number. Please guess between 1 and 100."
-    elif number_guess < random_number:
-        print "Your guess is too low, try again."
-    elif number_guess > random_number:
-        print "Your guess is too high, try again."
-    else:
-        print "Congrats! You guessed correctly, after %d guesses." % (count)
-        break
+        if number_guess < 0 or number_guess > 100:
+            print "That is not a valid number. Please guess between 1 and 100."
+        elif number_guess < random_number:
+            print "Your guess is too low, try again."
+        elif number_guess > random_number:
+            print "Your guess is too high, try again."
+        else:
+            print "Congrats! You guessed correctly, after %d guesses." % (count)
+            break
